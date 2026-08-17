e820c0a34ce556659f4b317d1c78ccb8c35788c5
Ihar <Garik666770@gmail.com>
2026-08-17 18:31:44 +0300

    Use static Open Graph image

--- src/app/opengraph-image.png/route.tsx
+++
@@ -1,8 +0,0 @@
-import { ImageResponse } from "next/og";
-import { OpenGraphImageMarkup, openGraphImageSize } from "@/lib/opengraph-image";
-
-export const runtime = "edge";
-
-export function GET() {
-  return new ImageResponse(<OpenGraphImageMarkup />, openGraphImageSize);
-}
